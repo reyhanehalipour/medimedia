c25c5078dd06d7f33f80962c78adba28ff428d01
reyhane <r.alipour@asax.ir>
2025-10-20 18:32:02 -0700

    fix ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,11 @@
 import HeroSection from "@/components/sections/HeroSection";
-
 import BestInStore from "@/components/sections/BestInStore";
-
 import AboutWatch from "@/components/sections/AboutWatch";
 import ProductGallery from "@/components/sections/ProductGallery";
 import Categories from "@/components/sections/Categories";
 import RecentBlog from "@/components/sections/RecentBlog";
-import Newsletter from "@/components/sections/Newsletter";
 import BrandExperience from "@/components/sections/BrandExperience";
-
-import Navbar from "@/components/Layout/Navbar";
 import Footer from "@/components/Layout/Footer";
-import Container from "@/components/Layout/Container";
 import PromoBanners from "@/components/sections/PromoBanners";
 import MostWanted from "@/components/sections/MostWanted";
 import Header from "@/components/Layout/Header";
